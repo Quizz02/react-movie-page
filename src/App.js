451e1af0c4d3228cc0e27915b34baf947b05236d
Juan de Dios Quiroz <quirozrodriguezj@gmail.com>
2023-01-25 23:10:07 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import PageWrapper from './PageWrapper';
+import Pelicula from './Pelicula';
+import peliculasJson from './peliculas.json';
 
 function App() {
+
+  let peliculas = peliculasJson;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageWrapper>
+
+      {peliculas.map(pelicula => {
+        return <Pelicula titulo={pelicula.titulo} calificacion={pelicula.calificacion} director={pelicula.director}
+          actores={pelicula.actores} fecha={pelicula.fecha} duracion={pelicula.duracion}
+          img={pelicula.img} descripcion={pelicula.descripcion}>
+        </Pelicula>
+      })}
+
+    </PageWrapper>
   );
 }
 
